aa0e5f0c75ce817f5a8026eb2d92c99029b8c22d
MaxOstapets <maksostapets2007@gmail.com>
2023-12-27 21:40:21 +0200

    Upgraded salaries check

--- signUp/signUp2.js
+++ signUp/signUp2.js
@@ -19,35 +19,40 @@ const sum_check = /[a-dA-D.*+?^${}()]/
 accept.addEventListener('click', ()=>{
     const user_man_name = man_name.value
     const user_woman_name = woman_name.value
-    
-    const user_man_salarie = parseFloat(man_salarie.value) 
-    const user_woman_salarie = parseFloat(woman_salarie.value)     
-    const sum_salaries = user_man_salarie + user_woman_salarie
 
-    const user_object = new Object()
-    user_object.man_object_name = user_man_name
-    user_object.woman_object_name = user_woman_name
-    user_object.man_object_salarie = user_man_salarie
-    user_object.woman_object_salarie = user_woman_salarie
+    // const sum_salaries = user_man_salarie + user_woman_salarie
 
-    objective.forEach((e) => objectives.push(e.value));
-    sum.forEach((e) => {
-        if(sum_check.test(e.value)){console.log('Error');}else{sums.push(e.value)}
-    });
-
-    for(let i = 0; i < 3; i++){
-        const user_object_objectives = new Object();
-        user_object_objectives.objective = objectives[i]
-        user_object_objectives.sum = sums[i]
-        users_objectives.push(user_object_objectives)
+    if (isNaN(man_salarie.value) || isNaN(woman_salarie.value)){
+        console.log("error");
+    }else{
+        const user_man_salarie = parseFloat(man_salarie.value)
+        const user_woman_salarie = parseFloat(woman_salarie.value)  
+        const sum_salaries = user_man_salarie + user_woman_salarie
+        
+        const user_object = new Object()
+        user_object.man_object_name = user_man_name
+        user_object.woman_object_name = user_woman_name
+        user_object.man_object_salarie = user_man_salarie
+        user_object.woman_object_salarie = user_woman_salarie    
+        console.log('Users object:', user_object);    
+        console.log('Загальна сума зарплати:', sum_salaries);
     }
+
+    // objective.forEach((e) => objectives.push(e.value));
+    // sum.forEach((e) => {
+    //     if(sum_check.test(e.value)){console.log('Error');}else{sums.push(e.value)}
+    // });
+
+    // for(let i = 0; i < 3; i++){
+    //     const user_object_objectives = new Object();
+    //     user_object_objectives.objective = objectives[i]
+    //     user_object_objectives.sum = sums[i]
+    //     users_objectives.push(user_object_objectives)
+    // }
     
-    console.log('Загальна сума зарплати:', sum_salaries);
-    console.log('Users object:', user_object);
-    console.log('Загальний масив:', users_objectives);
+    
+    // console.log('Загальний масив:', users_objectives);
 
     // localStorage.setItem('Information', JSON.stringify(users_objectives))
-
     
 })
-
