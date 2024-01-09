325b716dd7b0ee5d22627537f3e16df2ff4ec865
MaxOstapets <maksostapets2007@gmail.com>
2024-01-09 20:11:54 +0200

    add date function, addapt css and statistic js for profile

--- profile/profile.js
+++ profile/profile.js
@@ -1,17 +1,17 @@
-//INFO VARIABLES
+//INFO VARIABLES------------------------------------------------------------------
 const information_users_str = localStorage.getItem('information_users')
 const information_objectives_str = localStorage.getItem('information_objectives')
 const total_sum_str = localStorage.getItem('total_sum')
 const sum_array = []
 let sum_number = 0
 
-//PARSE VARIABLES
+//PARSE VARIABLES------------------------------------------------------------------
 const information_users = JSON.parse(information_users_str)
 const information_objectives = JSON.parse(information_objectives_str)
 const total_sum = JSON.parse(total_sum_str)
 let title_salarie = JSON.parse(total_sum_str)
 
-//QUERY SELECTOR VARIABLES
+//QUERY SELECTOR VARIABLES------------------------------------------------------------------
 const man_name_salarie = document.querySelector('.man_name_salarie')
 const woman_name_salarie = document.querySelector('.woman_name_salarie')
 let total_salarie_sum = document.querySelector('.total_salarie_sum')
@@ -19,12 +19,7 @@ const average_expense = document.querySelector('.average_expense')
 let sum = document.querySelector('.sum') 
 const objectives_sum = document.querySelector('.objectives_sum')
 
-//CREATE DIV WITH OBJECTIVES
-const main = document.querySelector('main')
-const objectives_div = document.createElement('div')
-objectives_div.classList.add('objectives')
-
-//AVERAGE EXPENSE && OBJECTIVES SUM
+//AVERAGE EXPENSE && OBJECTIVES SUM------------------------------------------------------------------
 function average_expense_calc(){
     for (let i = 0; i < sum_array.length; i++) {
         sum_number += sum_array[i]
@@ -35,6 +30,18 @@ function average_expense_calc(){
     average_expense.innerText = `Середня сума витрат: ${Math.floor(average_expense_num)} грн.`
     objectives_sum.innerText = `Сума витрат: ${sum_number} грн.`
 }
+
+//DATE FUNCTION------------------------------------------------------------------
+function date_fix(){
+    const date = new Date ()
+    const month = date.getMonth()
+    console.log("Місяць:", month);
+}
+
+//CREATE DIV WITH OBJECTIVES------------------------------------------------------------------
+const main = document.querySelector('main')
+const objectives_div = document.createElement('div')
+objectives_div.classList.add('objectives')
 
 function create_div_objective(e) {
     const objective_div = document.createElement('div')
@@ -68,18 +75,17 @@ function create_div_objective(e) {
         let minus_sum_salarie = title_salarie - e.user_sum
         title_salarie = minus_sum_salarie
 
-        console.log(minus_sum_number);
-        console.log(minus_sum_salarie);
-
         objectives_sum.innerText = `Сума витрат: ${minus_sum_number} грн.`
         check_icon.setAttribute('src', '../img/profile/checked_elipse.png')
         sum.innerText = `Загальна сума: ${minus_sum_salarie} грн.`
 
         objective_div.style.cssText = 'text-decoration: line-through; color: rgb(78, 78, 78); border: 3px solid rgb(78, 78, 78);'
+
+        date_fix()
     })
 }
 
-//INNER TEXT
+//INNER TEXT------------------------------------------------------------------
 man_name_salarie.innerText = `${information_users.man_name}: зарплата ${information_users.man_salarie} грн.`
 woman_name_salarie.innerText = `${information_users.woman_name}: зарплата ${information_users.woman_salarie} грн.`
 total_salarie_sum.innerText = `Сума зарплат разом: ${total_sum} грн.`
@@ -90,7 +96,7 @@ function create_objective(){information_objectives.forEach((e) => create_div_obj
 create_objective()
 average_expense_calc(sum_array) 
 
-//CONSOLE LOG INFORMATION
+//CONSOLE LOG INFORMATION------------------------------------------------------------------
 console.log("Array with sums:", sum_array);
 console.log("Information about users:", information_users)
 console.log("Information about objectives:", information_objectives)
